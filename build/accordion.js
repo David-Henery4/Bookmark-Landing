d1b49e2392ce788798201ed8fe2e862b3b370faa
DizzlyD <david4henery00@hotmail.com>
2022-11-22 20:51:52 +0000

    accordion&sidebar

---
+++ build/accordion.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// ALL CONTENT ELEMENTS
+const accordContent = document.querySelectorAll(".accord-content");
+// ALL ACCORDION ICONS
+const accordionIcons = document.querySelectorAll(".accord-icon");
+// ACCORDION CONTAINER
+const questionsContainer = document.getElementById("questions");
+
+questionsContainer.addEventListener("click", (e) => {
+  const currentQuest = e.target.closest(".accord");
+  if (!currentQuest.dataset.quest) return;
+  toggleFaqs(currentQuest);
+  flipAccordionIcon(currentQuest);
+});
+
+// ACCORDIONS
+const toggleFaqs = (currentQuest) => {
+  accordContent.forEach((e, i, a) => {
+    const content = e.dataset.questcontent;
+    // TOGGLE ACTIVE FAQ
+    if (content === currentQuest.dataset.quest) {
+      e.style.maxHeight === "500px"
+        ? (e.style.maxHeight = "0px")
+        : (e.style.maxHeight = "500px");
+    }
+    // CLOSE THE REST OF FAQs WHEN NOT ACTIVE
+    if (content !== currentQuest.dataset.quest) {
+      e.style.maxHeight = "0px";
+    }
+  });
+};
+
+// LABEL ICONS
+const flipAccordionIcon = (currentQuest) => {
+  accordionIcons.forEach((e, i, a) => {
+    const icon = e.dataset.icon;
+    // TOGGLES ACTIVE ICON
+    if (currentQuest.dataset.quest === icon) {
+      e.style.transform === "rotateX(0deg)"
+        ? (e.style.transform = "rotateX(180deg)")
+        : (e.style.transform = "rotateX(0deg)");
+    }
+    // RESETS NON-ACTIVE ICONS
+    if (icon !== currentQuest.dataset.quest) {
+      e.style.transform = "rotateX(0deg)";
+    }
+  });
+};
--- build/sidebar.js
+++ build/sidebar.js
@@ -1,12 +1,13 @@
+"use strict";
 
-const burger = document.getElementById("burger")
-const sidebar = document.getElementById("sidebar")
-const closeIcon = document.getElementById("close-icon")
+const burger = document.getElementById("burger");
+const sidebar = document.getElementById("sidebar");
+const closeIcon = document.getElementById("close-icon");
 
 burger.addEventListener("click", () => {
-  sidebar.style.transform = "translateX(0%)"
-})
+  sidebar.style.transform = "translateX(0%)";
+});
 
 closeIcon.addEventListener("click", () => {
   sidebar.style.transform = "translateX(100%)";
-})
+});
